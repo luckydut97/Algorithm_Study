c9a8dbb1d915dcaa0fde955b54b88129e14f16fe
luckydut97 <82084402+luckydut97@users.noreply.github.com>
2024-06-23 20:59:04 +0900

    [Bronze V] Title: A×B, Time: 208 ms, Memory: 17724 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,12 @@
+//import java.util.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int c=a*b;
+        System.out.print(c);
+    }
+}
