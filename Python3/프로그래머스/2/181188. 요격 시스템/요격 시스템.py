8f012fe839aca315b4bcae52f9d091a559347e5d
하상범 <82084402+luckydut97@users.noreply.github.com>
2024-11-22 21:06:17 +0900

    [level 2] Title: 요격 시스템, Time: 1282.32 ms, Memory: 89.4 MB -BaekjoonHub

--- Python3/프로그래머스/2/181188. 요격 시스템/요격 시스템.py
+++ Python3/프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -1,15 +1,29 @@
 def solution(targets):
+    
     # 종료 지점 기준으로 정렬
-    targets.sort(key=lambda x: x[1])
+    targets=sorted(targets)
+
     
-    # 요격 미사일 개수
-    missile_count = 0
-    last_missile_position = -1  # 마지막 요격 미사일 위치
+    answer=1 #요격횟수
+    aim_start, aim_end = targets.pop(0) #현재 조준범위
     
-    for s, e in targets:
-        # 현재 구간이 마지막 요격 미사일로 커버되지 않는 경우
-        if s >= last_missile_position:
-            missile_count += 1
-            last_missile_position = e  # 새로운 미사일 발사
+    
+    for target in targets:
+        #꺼내오기
+        target_start, target_end = target
+        
+        # 다음 범위의 최소 < 조준 범위의 최대: 요격횟수그대로
+        if target_start < aim_end:
+            #조준범위를 좁힌다.
+            aim_start= max(aim_start, target_start) #조준범위의 x좌표 갱신
+            aim_end= min(aim_end,target_end) #조준범위의 y좌표 갱신
             
-    return missile_count
+        else:
+            aim_start,aim_end=target_start, target_end
+            #요격횟수추가
+            answer+=1
+            
+        
+    
+        
+    return answer
